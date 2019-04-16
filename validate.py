82c5c6a3217769b8711edfc4aec89d57a6c7f5c4
wAx_100 <wakokara@wax100.io>
2019-04-17 01:10:29 +0900

    リファクタリング / データ追加

--- validate.py
+++ validate.py
@@ -3,41 +3,48 @@ import os
 import json
 import jsonschema
 
+OK = 0
+NG = 0
+CASE = 0
 
-LOOT_TABLE_JSON_DIR = "../playground/loot_tables/"
-LOOT_TABLE_SCHEMA = "../java/data/loot_table.json"
+LOOT_TABLE_JSON_DIR = "./playground/loot_tables/"
+LOOT_TABLE_SCHEMA = "./java/data/loot_table.json"
 
 def main():
+  # Loottable チェック
   valid_check(LOOT_TABLE_SCHEMA, LOOT_TABLE_JSON_DIR)
-
+# 
 
 def valid_check(schemaFile, dataDir):
   schema = json.load(open(schemaFile, 'r'))
 
   for pathname, dirnames, filenames in os.walk(dataDir):
-    for filename in filenames:
-        if filename.endswith('.json'):
-         try:
-          if(filename != None):
-            data = json.load(open(dataDir + filename, 'r'))
-            print ("Check: {} -> {}").format(schemaFile, dataDir + filename)
-            result = jsonschema.validate(data, schema)
-            if result == None:
-              print ("{} OK".format(filename))
-         except jsonschema.ValidationError as e: 
+    global OK
+    global NG
+    global CASE
+    for datafile in filenames:
+        if datafile.endswith('.json'):
+          try:
+            if(datafile != None):
+              CASE += 1
+              data = json.load(open(dataDir + datafile, 'r'))
+              print ("Check: {} -> {}").format(schemaFile, dataDir + datafile)
+              result = jsonschema.validate(data, schema)
+              if result == None:
+                OK += 1
+                print ("{} OK".format(datafile))
+          except jsonschema.ValidationError as e: 
+                NG += 1
                 print('Invalid JSON - {0}'.format(e.message))
-                print ("{} NG".format(filename))
+                print ("{} NG".format(datafile))
+          finally: print("")
         
 if __name__=='__main__':
     main()
-
+    print("Case: {0} - OK: {1} / NG: {2}").format(CASE, OK, NG)
+    if(CASE == OK + NG):
+      if(CASE == OK): exit(0)
+      else: exit(1)
 #TODO 各種リソース類のValidate用の呼び出しMethodを分けて作成する
 #TODO Json読み込み用Method作る
-#TODO OK / NG 数カウントする
-#TODO Exceptionは全てMainへ返して終了コードを 0 / 1 返す
 #TODO GitHubURL参照できるようにしとく
-
-#TODO 終了コードメモ
-# 0 正常終了
-# 1 NG終了
-# -1 例外(バリデート失敗時の例外は除く)終了
